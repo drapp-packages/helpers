d432fe3a086159c4aaad3f800fb1329b5246c185
Ricky <ricardo.kleine.samson@gmail.com>
2020-08-01 09:08:19 -0300

    Add Sandbox

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ helpers.getEmail = address => {
 helpers.isEmail = string => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(string)
 
 helpers.getName = value => {
-  return (value || '').toString().toLowerCase().replace(/[^A-Za-zÀ-ÖØ-öø-ÿ ]/g, '').replace(/\s\s+/g, ' ').split(' ').reduce((array, word) => {
-    if (word.length === 1 || ['di', 'de', 'del'].includes(word.toLowerCase())) return [...array, word]
-    const part = (word[0] || '').toUpperCase() + word.slice(1)
-    return [...array, part]
-  }, []).join(' ')
+  try {
+    const dot = /\s+([A-Za-zÀ-ÖØ-öø-ÿ]{1}\.$)/.test(value)
+    return (value || '').toString().replace(/[^A-Za-zÀ-ÖØ-öø-ÿ ]/g, '').replace(/\s\s+/g, ' ').split(' ').reduce((array, word) => {
+      if (word.length === 1) return [...array, word.toUpperCase()]
+      if (['di', 'de', 'del', 'la'].includes(word.toLowerCase())) return [...array, word]
+      const part = (word[0] || '').toUpperCase() + word.slice(1).toLowerCase()
+      return [...array, part]
+    }, []).join(' ') + (dot ? '.': '')
+  } catch {}
+  return value
 }
 
 module.exports = helpers
